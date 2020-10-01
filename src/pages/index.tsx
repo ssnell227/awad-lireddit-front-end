4cfbcbb3086e107b2d16d780ac76eb5eae8ac8c7
ssnell227 <ssnell227@gmail.com>
2020-10-01 14:53:57 -0600

    adds next.js app with chakra

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import {
+  Link as ChakraLink,
+  Text,
+  Code,
+  Icon,
+  List,
+  ListIcon,
+  ListItem,
+} from '@chakra-ui/core'
+
+
+
+const Index = () => (
+  <div>
+    hello world
+  </div>
+)
+
+export default Index
